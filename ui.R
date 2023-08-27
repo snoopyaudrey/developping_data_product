cd27afb930b6fb9952515fb9a246b8693a274a80
snoopyaudrey <121604214+snoopyaudrey@users.noreply.github.com>
2023-08-27 13:24:50 +0200

    ui.R

--- ui.R
+++ ui.R
@@ -1,13 +1,13 @@
 ui <- fluidPage(
- 
-  titlePanel("Simple Mean Calculator"),
+  titlePanel("Mean Calculator by Species"),
   sidebarLayout(
     sidebarPanel(
       selectInput("variable", "Select a Variable:", choices = colnames(iris[1:length(iris)-1])),
+      selectInput("species", "Select a Species:", choices = unique(iris$Species)),
       actionButton("calculate", "Calculate Mean")
     ),
     mainPanel(
-      verbatimTextOutput("app_description"),
+      verbatimTextOutput("app_description"),  # Display explanatory text
       textOutput("result")
     )
   )
